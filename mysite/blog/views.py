e2206622a49eb64693c8f355535b59854b587903
greenmac <alwaysmac@msn.com>
2018-09-28 00:29:19 +0800

    12

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from .models import Blog, BlogType
 def blogList(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 def blogDetail(request, blog_pk):
@@ -15,6 +16,7 @@ def blogDetail(request, blog_pk):
 def blogsWithType(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
