5d0075a0597cabff8c6bb13cd4f6e0d49bd72cfc
greenmac <alwaysmac@msn.com>
2018-09-30 21:22:08 +0800

    Heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,14 +79,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'hulk',
+        'PASSWORD': 'hulk1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -146,3 +157,8 @@ CACHES = {
         'LOCATION': 'my_cache_table',
     }
 }
+
+import os
+if os.getenv('DATABASE_URL') is not None:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
